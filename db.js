646cd4ad1fb645c2458d58c2e15474c568d79237
Daewis <davidabokunwa@gmail.com>
2025-09-20 23:35:25 +0100

    Initial commit - add LSMS project files

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 // db.js
 import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 const { Pool } = pg;
 
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'LASUED SIWES MANAGEMENT SYSTEM', // Double-check this exact name matches your PostgreSQL DB
-    password: 'Abokunwa20',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10),
 });
 
 // Optional: Test the connection on file load.
